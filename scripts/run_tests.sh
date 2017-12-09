d0c592ec9975755d828995fa596295bd485b35ed
Marty Jones <murtin.jones@gmail.com>
2017-12-09 10:51:48 -0600

    use run_scripts.sh

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -4,9 +4,31 @@
 # In case one test crashes without cleaning up the containers, 
 # it does it after the tests have been run
 
-echo "Running tests"
-jasmine-node --verbose --captureExceptions test
-exit_code=$?
+integration_tests() {
+    echo "Running integration tests"
+    jasmine-node --verbose --captureExceptions test
+    exit_code=$?
+}
+
+unit_tests() {
+    echo "Running unit tests"
+    node_modules/.bin/mocha test/unit/** --require babel-core/register
+    exit_code=$?
+}
+
+exit_code=1
+for arg in "$@"; do
+    if [[ $arg == "unit" ]]; then
+        unit_tests
+    elif [[ $arg == "integration" ]]; then
+        integration_tests
+    fi
+
+    if [ $exit_code != 0 ]; then
+	echo "Tests failed"
+	exit $exit_code
+    fi
+done
 
 old_containers=$(docker ps -aq --filter label=__docker_sandbox)
 if [ ! -z "$old_containers" ]; then
